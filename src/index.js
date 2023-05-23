3128aa16db23666e0b2d471cec563f028c1b6864
lavparker <lavinia.parker@outlook.com>
2023-05-23 11:38:41 -0400

    edited countrynametarget

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
       svg.on('mouseover', (d) => {
         // console.log('MOUSEOVER EVENT', d.target?.dataset);
         let selected_country = getCountryNameFromTarget(d.target?.dataset);
-
+        console.log(d.target); 
         if (nationalFoods[selected_country]) {
           document.getElementsByClassName("country").innerHTML = selected_country;
         }
