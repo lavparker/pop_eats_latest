636eb10b076ce341a45878bdf27ef0ef9bc7bc31
lavparker <lavinia.parker@outlook.com>
2023-05-31 00:07:02 -0400

    editting css

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ document.addEventListener("DOMContentLoaded", () => {
   closeModalBtn.addEventListener("click", closeModal);
   overlay.addEventListener("click", closeModal);
 
-  const width = 900;
-  const height = 900;
+  const width = 960;
+  const height = 600;
 
   const svg = d3.select('body')
     .append('svg')
