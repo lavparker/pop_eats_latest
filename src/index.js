0dafd2a6a635f45ed9da3f557a32d2be4c525ae9
lavparker <lavinia.parker@outlook.com>
2023-05-30 20:55:03 -0400

    editting css

--- src/index.js
+++ src/index.js
@@ -39,8 +39,8 @@ document.addEventListener("DOMContentLoaded", () => {
 // const path = d3.geoPath().projection(projection);
 
 // const tooltip = svg.append('g');
-  const width = 400;
-  const height = 400;
+  const width = 350;
+  const height = 350;
 
   const svg = d3.select('body')
     .append('svg')
